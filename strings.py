a565b412e4b7e20d6c7bbd2e2e39506879caef89
Amrutha <amruthapurnavadrevu@Amruthas-MacBook-Pro.local>
2025-01-19 16:22:15 +0530

    Strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+my_string = "airconditioner"
+
+print(f"Capital Letters: {my_string.upper()}")
+print(f"Small Letters: {my_string.lower()}")
+print(f"Count number of occurrences of letter 'o': {my_string.count('o')}")
+print(f"Index of first occurrence of letter 'n':{my_string.find('n')}")
+print(f"Centering the string: {my_string.center(30)}")
+print(f"Left justifying: {my_string.ljust(30)}")
+print(f"Right justifying: {my_string.rjust(30)}")
+print(f"Split: {my_string.split('r')}")
