5e8a8accb6abe5809e0c9df1ea7fe27da6674790
Amrutha <amruthapurnavadrevu@Amruthas-MacBook-Pro.local>
2025-01-25 15:27:06 +0530

    Strings

--- string_formatting.py
+++ string_formatting.py
@@ -1,9 +1,16 @@
 print("hello")
 
-print("Hello","World")
+print("Hello","World") #Blank is the default separator
 
 print("Hello","World",sep="***")
 
 print("Hello","World",end="***\n")
 
 print("Hello",end="***");print("World")
+
+#f-strings
+
+height = 156.89
+is_tall = True
+
+print(f"Your height is {height} and it is {str(is_tall).lower()} that you are tall.")
